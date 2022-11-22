5157497f44860a02fc754b42af8f2185c80dd12f
user <diaranarbekov430@mail.ru>
2022-11-22 09:34:19 +0500

    fix bug

--- js/themeClick.js
+++ js/themeClick.js
@@ -1,5 +1,4 @@
 function hoverTitles(){
-    let array1 = [1, 2, 3]
     let titles = document.querySelectorAll('.hover__title-1')
     let arrayTitles = Array.from(titles)
     console.log(arrayTitles);
@@ -9,9 +8,6 @@ function hoverTitles(){
             element.classList.add('h1__active')
             element.classList.remove('hover__title-1')
         });
-        // arrayTitles.forEach().classList.add('h1__active')
-        // arrayTitles.classList.add('h1__active');
-        // arrayTitles.classList.remove('hover__title-1')
     }, 500);
 }
 
